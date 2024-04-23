43518cb9655c0ef6ced4b0fc38cbcac7a6d6b724
Heli135 <164352976+Hlm19966@users.noreply.github.com>
2024-04-23 11:20:19 +0330

    added function

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function displyWeather(response) {
+    console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = response.data.temperature.current;
-    
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = response.data.condition.description;
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 }
 
 
@@ -28,6 +32,7 @@ function handlesearchsubmit(event) {
     let cityElement = document.querySelector("#city");
     let formElement = document.querySelector("#search-form-input");
     cityElement.innerHTML = formElement.value;
+    searchingCity(formElement.value);
 }
 
  
